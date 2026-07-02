75d80220876a67e33b2febaf535984120391deb2
Muchammad Rofky <166226114+MrHaz354@users.noreply.github.com>
2026-07-02 10:18:44 +0700

    fix: force prisma generate before next build

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Memaksa Vercel untuk tetap melanjutkan build meskipun ada warning/error tipe data TS
+    // Memaksa Vercel untuk tetap melanjutkan build meskipun ada warning tipe data TS
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Memaksa Vercel mengabaikan warning ESLint (seperti variabel tak terpakai)
-    ignoreDuringBuilds: true,
   },
 };
 
